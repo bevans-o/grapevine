1b148ded744d4a7b57c891e7d97fb78a482e9193
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-08-25 21:02:25 +1000

    routing setup

---
+++ src/app/tool/build/[vine]/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Build({ params }: {params: {vine: string}} ) {
+    return (
+      <main>
+        Build: {params.vine}
+      </main>
+    )
+  }
+  
---
+++ src/app/tool/edit/[vine]/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Edit({ params }: {params: {vine: string}} ) {
+    return (
+      <main>
+        Edit: {params.vine}
+      </main>
+    )
+  }
+  
