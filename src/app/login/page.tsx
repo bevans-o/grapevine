8eb1b16f0207e4664d25cd6ab81cd969b24c181f
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-08-25 21:26:10 +1000

    logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,15 @@
+import Logo from "../components/Logo/Logo";
+import login from "./login.module.css"
 
 export default function Login() {
     return (
-      <main>
-        Login
+      <main className={login.main}>
+        <div className={login.content}>
+          <Logo />
+          <div className={login.box}>
+            Login
+          </div>
+        </div>
       </main>
     )
   }
