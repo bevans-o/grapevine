f7bd76c31a7968f710ccc806a5665ca5ea67807d
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-08-27 02:47:36 +1000

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,9 @@ import {getUser, getAllUsers, getAllVines, getVine} from '../lib/functions';
 import { useEffect, useState } from "react";
 import {User, Vine} from '../lib/types'
 import dashboard from './dashboard.module.css'
+import UserBubble from "../components/User/User";
+import Logo from "../components/Logo/Logo";
+import Names from "../components/Names/Names";
 
 export default function Dashboard() {
   const {data: session, status} = useSession();
@@ -30,18 +33,44 @@ export default function Dashboard() {
 
   return (
     <main className={dashboard.main}>
-      Dashboard
-      <p>
-        {JSON.stringify(vines)}
-      </p>
+      <div className={dashboard.grid}>
+        <div className={dashboard.logo}><Logo type="full"/></div>
+        <h1 className={dashboard.heading}>Dashboard</h1>
 
-      <p>
-        {JSON.stringify(user)}
-      </p>
+        <div className={dashboard.voteVines}>
+          Voting Open
+          <div className={dashboard.vineContainer}>
+            {vines?.map((vine) => 
+              <div key={vine.id} className={dashboard.vine}>{vine.name.slice(0, 1)}</div>
+            )}
+          </div>
+        </div>
 
-      <p>
-        {JSON.stringify(users)}
-      </p>
+        <div className={dashboard.editVines}>
+          Your Vines
+
+          <div className={dashboard.vineContainer}>
+            {vines?.map((vine: any) => {
+              if (vine.owner === user?.email || vine.owner.email === user?.email ) {
+                return <div key={vine.id} className={dashboard.vine}>{vine.name.slice(0, 1)}</div>
+              }
+            })}
+          </div>
+        </div>
+
+        <div className={dashboard.users}>
+          Your Team
+
+          <div className={dashboard.userBubbles}>
+            {users.map((user) => 
+              <UserBubble key={user.email} user={user} vote="yes"/>
+            )}
+          </div>
+          
+        </div>
+      </div>
+
+      <Names />
     </main>
   )
 }
