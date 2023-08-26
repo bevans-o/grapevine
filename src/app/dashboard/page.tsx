c966b79909a9fd36ec4b03044103649488bb43e8
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-08-27 03:25:34 +1000

    dashboard done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import {useSession} from "next-auth/react";
-import {getUser, getAllUsers, getAllVines, getVine} from '../lib/functions';
+import {getUser, getAllUsers, getAllVines, getVine, createVine} from '../lib/functions';
 import { useEffect, useState } from "react";
 import {User, Vine} from '../lib/types'
 import dashboard from './dashboard.module.css'
 import UserBubble from "../components/User/User";
 import Logo from "../components/Logo/Logo";
 import Names from "../components/Names/Names";
+import Link from "next/link";
+import Button from "../components/Button/Button";
+import router from "next/router";
 
 export default function Dashboard() {
   const {data: session, status} = useSession();
@@ -30,36 +33,43 @@ export default function Dashboard() {
       }).catch((error) => console.log(error))
   }, [])
 
+  const handleNew = () => {
+    createVine(user!).then(
+        (res) => router.push("/tool/build/" + res.toString())
+    )
+};
+
 
   return (
     <main className={dashboard.main}>
       <div className={dashboard.grid}>
-        <div className={dashboard.logo}><Logo type="full"/></div>
+        <div className={dashboard.logo}><Logo type="full"/> <span>Enterprise Edition</span></div>
         <h1 className={dashboard.heading}>Dashboard</h1>
 
         <div className={dashboard.voteVines}>
-          Voting Open
+          <h2>Voting Open</h2>
           <div className={dashboard.vineContainer}>
-            {vines?.map((vine) => 
-              <div key={vine.id} className={dashboard.vine}>{vine.name.slice(0, 1)}</div>
+            {vines?.map((vine: any) => 
+              <Link href={'/tool/vote/' + vine._id}><div key={vine.id} className={dashboard.vine}>{vine.name.slice(0, 1)}<div className={dashboard.vineTitle}>Vote on {vine.name}</div></div></Link>
             )}
           </div>
         </div>
 
         <div className={dashboard.editVines}>
-          Your Vines
+          <h2>Your Vines <Button text={'New Vine'} type="" onClick={() => handleNew()}></Button></h2>
 
           <div className={dashboard.vineContainer}>
             {vines?.map((vine: any) => {
+              console.log(vine);
               if (vine.owner === user?.email || vine.owner.email === user?.email ) {
-                return <div key={vine.id} className={dashboard.vine}>{vine.name.slice(0, 1)}</div>
+                return <Link href={'/tool/build/' + vine._id}><div key={vine.id} className={dashboard.vine}>{vine.name.slice(0, 1)} <div className={dashboard.vineTitle}>Edit {vine.name}</div></div></Link>
               }
             })}
           </div>
         </div>
 
         <div className={dashboard.users}>
-          Your Team
+          <h2>Your Team</h2>
 
           <div className={dashboard.userBubbles}>
             {users.map((user) => 
