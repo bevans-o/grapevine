4e18c2e733ab72969a29936fe47e77e047425bc7
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-08-27 01:46:05 +1000

    login page styling

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import button from './button.module.css'
 
-function Button({text, type="", onClick}: {
+function Button({text, type="", onClick, children}: {
     text: string,
     type?: string,
-    onClick: Function
+    onClick: Function,
+    children?: React.ReactNode
 }) {
 
 
   return (
     <button className={`${button.base} ${type === "yes" && button.yes} ${type === "no" && button.no}`} onClick={() => onClick()}>
-        <span className={button.text}>{text}</span>
+        <span className={button.text}>{text}</span>{children}
     </button>
   )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,8 @@
 import Logo from "../components/Logo/Logo";
 import login from "./login.module.css"
 import { signIn, signOut, useSession } from "next-auth/react";
+import GoogleIcon from '@mui/icons-material/Google';
+import Button from "../components/Button/Button";
 
 
 export default function Login() {
@@ -10,10 +12,13 @@ export default function Login() {
       <main className={login.main}>
         <div className={login.content}>
           <Logo type="full"/>
+          <div className={login.welcome}>
+            Welcome! <strong>Grapevine</strong> is a tool to support collective decision-making in the workplace.
+          </div>
           <div className={login.box}>
-            <button onClick={() => signIn("google", {callbackUrl: "http://localhost:3000/dashboard"})}>
-            Login
-            </button>
+            <Button text={"Login with Google"} type="yes" onClick={() => signIn("google", {callbackUrl: "http://localhost:3000/dashboard"})}>
+              <GoogleIcon />
+            </Button>
           </div>
         </div>
       </main>
