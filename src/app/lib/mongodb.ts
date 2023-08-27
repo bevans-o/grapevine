443bd08fd8666d53fcbf773dd2f245c2f4ceaec8
risamp <risapais@gmail.com>
2023-08-27 11:04:22 +1000

    remove side hos

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -1,8 +1,5 @@
 import { MongoClient, Db } from "mongodb";
 
-export const client = new MongoClient(process.env.MONGODB_URI!);
-export const mongodb = client.connect();
-export const db = client.db("grapevine");
 
 
 class MongoGlobal {
